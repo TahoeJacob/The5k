ad68ea64a43b24a3a3b2ac0d4ba46a80be00ab63
jas371 <jas371@uclive.ac.nz>
2024-07-13 15:48:52 +1200

    Most recent update, finally setup main PC so will transfer to primarily working on this

--- CEAParser.py
+++ CEAParser.py
@@ -16,7 +16,8 @@ import pandas as pd
 import math
 
 # Importing the data from text file
-textfile = '/Users/jacobsaunders/Desktop/EngineDev/The5k/natetest.txt'
+testFileName = '/Users/jacobsaunders/Desktop/EngineDev/The5k/RawCSV/CEATextTest.txt'
+textfile = testFileName#'/Users/jacobsaunders/Desktop/EngineDev/The5k/Archive/natetest.txt'
 
 # Formatting text file
 data = pd.read_csv(textfile, sep='\t', header=None)
@@ -124,22 +125,38 @@ CEAData = {
         'Ivac': [],
         'Isp': [],
         }
+
 # Key rows
+# For Nate_Test data (for some reason there is an added row in 17 so everything is behind CEAData.txt)
 keyRows = [8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,25,27,28,29,31,32,33,6,35,36,37,38,39]
 TwoValueRows = [8,9,10,12,13,14,15,16,17,18,19,20,21,22,25,27,28,29,31,32,33]
 SingleValueRows = [6,35, 36, 37, 38, 39]
+
+# Rows for CEATest data
+# keyRows = [8,9,10,11,12,13,14,15,16,18,19,20,21,22,23,26,28,29,30,32,33,34,6,36,37,38,39,40]
+# TwoValueRows = [8,9,10,12,13,14,15,16,18,19,20,21,22,23,26,28,29,30,32,33,34]
+# SingleValueRows = [6, 36, 37, 38, 39, 40]
 CustomRows = [11]
 keysDict = list(CEAData.keys())
+print(len(keysDict))
 
+combinedData = pd.DataFrame()
 # Iterate through each sub_df and extract the data
 for key, sub_df in sub_dfs.items():
 
+    combinedData = pd.concat([combinedData, sub_df], ignore_index=False)
+
     # Dictionary index
     dictIndex = 0
+    # Depending on each row, extract the data and append to the dictionary
+    # Iterate through each row of the data
     for keyRowID in keyRows:
         firstValue = False
         skipCol = False
+        # Iterate through all columns excluding first 3 
         for columnID in sub_df.columns[3:]:
+
+            # If row has a Chamber and Exit value then check it is not empty
             if keyRowID in TwoValueRows:
                 if sub_df.iloc[keyRowID, columnID] != '' and sub_df.iloc[keyRowID, columnID] != None and firstValue == False and not sub_df.iloc[keyRowID, columnID].isalpha():
                     dictKeyID = keysDict[dictIndex]
@@ -175,38 +192,61 @@ for key, sub_df in sub_dfs.items():
                 if sub_df.iloc[keyRowID, columnID] != '' and sub_df.iloc[keyRowID, columnID] != None and not sub_df.iloc[keyRowID, columnID].isalpha():
                     dictKeyID = keysDict[dictIndex]
                     CEAData[dictKeyID].append(sub_df.iloc[keyRowID, columnID])
-                    #print(sub_df.iloc[keyRowID, columnID], dictKeyID, keyRowID, dictIndex)
+                    # print(sub_df.iloc[keyRowID, columnID], dictKeyID, keyRowID, dictIndex)
                     dictIndex +=1
                     break
         # After iterating through each column move onto next row and thus next dictionary index position         
-
     # Initialize default values for keys with '_Chamber' and '_Throat' suffixes
+    key_IDs = []
     for index, row in sub_df.iloc[41:].iterrows():
         key_ID = str(sub_df.iloc[index, 1]).strip()
+        print(key_ID)
         if key_ID == '':
+            # Need to make sure any keys not used in this sub df but used in previous sub dfs have none added to them for this round
+            keysDict = list(CEAData.keys())
+            used_key_IDs = keysDict[50:]
+            # Iterate through the current list of keys in the dictionary and compare against keys used in sub_df
+            for key in used_key_IDs:
+                if key not in key_IDs:
+                    CEAData[key].append(None)
+                    print(key)
             break
+        # Need to track what key_IDs have been used for this specific Sub_Df
         if key_ID + '_Chamber' not in CEAData:
             CEAData[key_ID + '_Chamber'] = [None] * (len(CEAData['O/F'])-1)
             CEAData[key_ID + '_Throat'] = [None] * (len(CEAData['O/F'])-1)
             #print(CEAData[key_ID + '_Chamber'])
 
-        molarMass_Chamber = ''
-        molarMass_Throat = ''
+        molarMass_Chamber = None
+        molarMass_Throat = None
         if len(key_ID) == 2:
-            molarMass_Chamber = sub_df.iloc[index, 15]
-            molarMass_Throat = sub_df.iloc[index, 16]
+            molarMass_Chamber = sub_df.iloc[index, 16]
+            molarMass_Throat = sub_df.iloc[index, 18]
         elif len(key_ID) == 3:
-            molarMass_Chamber = sub_df.iloc[index, 14]
-            molarMass_Throat = sub_df.iloc[index, 15]
+            molarMass_Chamber = sub_df.iloc[index, 15]
+            molarMass_Throat = sub_df.iloc[index, 17]
         elif len(key_ID) == 4:
-            molarMass_Chamber = sub_df.iloc[index, 13]
+            molarMass_Chamber = sub_df.iloc[index, 14]
+            molarMass_Throat = sub_df.iloc[index, 16]
+        elif len(key_ID) == 6:
+            molarMass_Chamber = sub_df.iloc[index, 12]
             molarMass_Throat = sub_df.iloc[index, 14]
+
         CEAData[key_ID + '_Chamber'].append(molarMass_Chamber)
         CEAData[key_ID + '_Throat'].append(molarMass_Throat)
 
+        # Append used key id with suffixes for this sub df
+        key_IDs.append(key_ID+'_Chamber')
+        key_IDs.append(key_ID+'_Throat')
+
+# Create a function to display the length off all arrays
+for key, value in CEAData.items():
+    print(key, len(value))
+
+combinedData.to_excel('combinedData.xlsx', index=True)
 # Export the data to an Excel file
 df = pd.DataFrame(CEAData)
-df.to_excel('output2.xlsx', index=False)
+df.to_excel('CEAParsed.xlsx', index=False)
 
 # Apply the function to all elements of the DataFrame
 df = df.applymap(custom_float_conversion)
--- MixtureOptimization.py
+++ MixtureOptimization.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 # Import excel file
-df = pd.read_excel('/Users/jacobsaunders/Desktop/EngineDev/The5k/output2.xlsx')
+df = pd.read_excel('/Users/jacobsaunders/Desktop/EngineDev/The5k/CEAParsed.xlsx')
 
 
 def create_plot(x_axis,y_axis, x_label, y_label, title):
@@ -16,16 +16,30 @@ def create_plot(x_axis,y_axis, x_label, y_label, title):
     plt.title(f'{title}')
     plt.grid(True)
     
+def calc_exit_mach_num(df):
+    # Calculate the mach number at the exit of the nozzle
+    
+    # Use the following equation to calculate the exit mach number
+    # M_e = sqrt(2/(gamma - 1) * ((P_c/P_e)^((gamma - 1)/gamma) - 1))
+
+def engine_geometry(df):
+    # Calculate the geometry of a rocket engine based off key parameters from CEA
+
+    # First step is to calculate mach numbers in the exit
+
 
 def main():
+    # Main function
 
+    # Calculate the engine geometry based off CEA data
+    engine_geometry(df)
 
-    result = (df['CF'] * df['Cstar']) / 9.81
-    create_plot(df['O/F'], df['T_Chamber'], 'O/F', 'T Chamber', 'T Chamber vs O/F')
-    create_plot(df['O/F'], df['Gamma_Chamber'], 'O/F', 'Gamma Chamber', 'Gamma Chamber vs O/F')
-    create_plot(df['O/F'], df['Cstar'], 'O/F', 'CStar', 'CStar vs O/F')
-    create_plot(df['O/F'], df['Isp'], 'O/F', 'Isp', 'Isp vs O/F')
-    create_plot(df['O/F'], result, 'O/F', 'Result', 'Result vs O/F')
-    plt.show()
+    # result = (df['CF'] * df['Cstar']) / 9.81
+    # create_plot(df['O/F'], df['T_Chamber'], 'O/F', 'T Chamber', 'T Chamber vs O/F')
+    # create_plot(df['O/F'], df['Gamma_Chamber'], 'O/F', 'Gamma Chamber', 'Gamma Chamber vs O/F')
+    # create_plot(df['O/F'], df['Cstar'], 'O/F', 'CStar', 'CStar vs O/F')
+    # create_plot(df['O/F'], df['Isp'], 'O/F', 'Isp', 'Isp vs O/F')
+    # create_plot(df['O/F'], result, 'O/F', 'Isp [Sec]', 'Isp vs O/F')
+    # plt.show()
 
 main()
